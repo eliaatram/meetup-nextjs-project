3798b040ab0c3dab1266d664ea64051085d8f2d6
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2021-10-11 18:43:04 +0200

    Update MeetupForm.js

--- components/Meetup/MeetupForm.js
+++ components/Meetup/MeetupForm.js
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import Card from '../ui/Card';
-import './MeetupForm.module.css';
-
+import classes from './MeetupForm.module.css';
 
 const NewMeetupForm = (props) => {
     const titleInputRef = useRef();
@@ -29,20 +28,20 @@ const NewMeetupForm = (props) => {
 
     return (
         <Card>
-            <form className='form' onSubmit={submitHandler}>
-                <div className='control'>
+            <form className={classes.form} onSubmit={submitHandler}>
+                <div className={classes.control}>
                     <label htmlFor='title'>Meetup Title</label>
                     <input type='text' required id='title' ref={titleInputRef} />
                 </div>
-                <div className='contorl'>
+                <div className={classes.control}>
                     <label htmlFor='image'>Meetup Image</label>
                     <input type='url' required id='image' ref={imageInputRef} />
                 </div>
-                <div className='control'>
+                <div className={classes.control}>
                     <label htmlFor='address'>Address</label>
                     <input type='text' required id='address' ref={addressInputRef} />
                 </div>
-                <div className='control'>
+                <div className={classes.control}>
                     <label htmlFor='description'>Description</label>
                     <textarea
                         id='description'
@@ -51,7 +50,7 @@ const NewMeetupForm = (props) => {
                         ref={descriptionInputRef}
                     ></textarea>
                 </div>
-                <div className='actions'>
+                <div className={classes.actions}>
                     <button>Add Meetup</button>
                 </div>
             </form >
